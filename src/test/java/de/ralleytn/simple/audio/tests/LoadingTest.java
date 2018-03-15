d352ffe1682e8984ddbc6f70e020327486e696e9
Ralph Niemitz <ralph.niemitz@gmx.de>
2018-03-15 07:17:32 +0100

    15.03.2018
    Signed-off-by: Ralph Niemitz <ralph.niemitz@gmx.de>

---
+++ src/test/java/de/ralleytn/simple/audio/tests/LoadingTest.java
@@ -0,0 +1,36 @@
+package de.ralleytn.simple.audio.tests;
+
+import org.junit.jupiter.api.Test;
+
+class LoadingTest {
+
+	@Test
+	void testAiff() {
+		
+		
+	}
+	
+	@Test
+	void testOgg() {
+		
+		
+	}
+	
+	@Test
+	void testAu() {
+		
+		
+	}
+	
+	@Test
+	void testWave() {
+		
+		
+	}
+	
+	@Test
+	void testMp3() {
+		
+		
+	}
+}
